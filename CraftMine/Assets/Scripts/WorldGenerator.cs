aac8903901b6adc0c99b40c4e292b869e0a247aa
mikepod9 <mikepodimov@gmail.com>
2017-09-16 12:37:09 -0400

    Added more terrain types as well as a way to scan through layers

--- CraftMine/Assets/Scripts/WorldGenerator.cs
+++ CraftMine/Assets/Scripts/WorldGenerator.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class WorldGenerator : MonoBehaviour {
 
+    public int seed;
+
     public int width = 256;
     public int height = 256;
+    public int maxDepth = 256;
     public float scale = 20f;
-    public float depth = 0;
+    public int depth = 0;
+
+    [Range(1,5)]
+    public int octaves = 4;
+    [Range(0,1)]
+    public float persistance = 0.5f;
+    [Range(0, 3)]
+    public float lacunarity = 2f;
 
     private float[,] map;
-
-	// Use this for initialization
+    
 	void Update () {
         GameObject quad = GameObject.Find("Quad");
         Renderer renderer = quad.GetComponent<Renderer>();
@@ -20,13 +29,53 @@ public class WorldGenerator : MonoBehaviour {
 	}
 
     void GenerateMap() {
+
         map = new float[width, height];
 
-        for(int x = 0; x < width; x++) {
+        Vector2[] octaveOffSets = new Vector2[octaves];
+        System.Random prng = new System.Random(seed);
+        for (int i =0; i < octaves; i++) {
+            float offSetX = prng.Next(-10000, 10000);
+            float offSetY = prng.Next(-10000, 10000);
+            octaveOffSets[i] = new Vector2(offSetX, offSetY);
+        }
+
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
+        for (int x = 0; x < width; x++) {
             for(int y = 0; y < height; y++) {
-                map[x, y] = Mathf.PerlinNoise((float)x / width * scale, (float)y / height * scale) + depth;
+
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
+
+                for (int i = 0; i < octaves; i++) {
+                    float sampleX = x / scale * frequency + octaveOffSets[i].x;
+                    float sampleY = y / scale * frequency + octaveOffSets[i].y;
+
+                    float noiseValue = Mathf.PerlinNoise(sampleX, sampleY) * maxDepth + depth;
+                    noiseHeight += noiseValue * amplitude;
+
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+                }
+
+                if (noiseHeight > maxNoiseHeight) {
+                    maxNoiseHeight = noiseHeight;
+                } else if (noiseHeight < minNoiseHeight) {
+                    minNoiseHeight = noiseHeight;
+                }
+
+                map[x, y] = noiseHeight;
             }
         }
+        //for (int y = 0; y < height; y++) {
+        //    for (int x = 0; x < width; x++) {
+        //        map[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, map[x, y]);
+        //    }
+        //}
+
     }
 
     Texture GenerateTexture() {
@@ -35,6 +84,8 @@ public class WorldGenerator : MonoBehaviour {
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 Color color = TerrainType(x,y);
+                texture.filterMode = FilterMode.Point;
+                texture.wrapMode = TextureWrapMode.Clamp;
                 texture.SetPixel(x, y, color);
             }
         }
@@ -44,12 +95,17 @@ public class WorldGenerator : MonoBehaviour {
     }
 
     Color TerrainType(int x, int y) {
-
         float type = map[x, y];
-        if(type <= 0.4) {
+
+        if(type <= 0.4 * maxDepth) {
             return Color.blue;
-        } else {
+        }
+        if (type <= 0.6 * maxDepth) {
             return Color.green;
         }
+        if (type <= 0.8 * maxDepth) {
+            return Color.black;
+        }
+        return Color.white;
     }
 }
