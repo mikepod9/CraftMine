5480e5dabe17b0cbdbcfccd15819a393da05f573
mikepod9 <mikepodimov@gmail.com>
2017-09-16 19:20:32 -0400

    Implementation of proper noise function that allows for 3D noise functions

---
+++ CraftMine/Assets/Editor/TextureCreatorINspector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(WorldGenerator))]
+public class TextureCreatorINspector : Editor {
+
+    private WorldGenerator noise;
+
+    private void OnEnable() {
+        noise = target as WorldGenerator;
+        Undo.undoRedoPerformed += RefreshNoise;
+    }
+
+    private void OnDisable() {
+        Undo.undoRedoPerformed -= RefreshNoise;
+    }
+
+    private void RefreshNoise() {
+        if (Application.isPlaying) {
+            noise.FillTexture();
+        }
+    }
+
+    public override void OnInspectorGUI() {
+        EditorGUI.BeginChangeCheck();
+        DrawDefaultInspector();
+        if (EditorGUI.EndChangeCheck()) {
+            RefreshNoise();
+        }
+    }
+}
--- CraftMine/Assets/Scripts/WorldGenerator.cs
+++ CraftMine/Assets/Scripts/WorldGenerator.cs
@@ -4,108 +4,70 @@ using UnityEngine;
 
 public class WorldGenerator : MonoBehaviour {
 
-    public int seed;
+    [Range(2, 512)]
+    public int resolution = 128;
 
-    public int width = 256;
-    public int height = 256;
-    public int maxDepth = 256;
-    public float scale = 20f;
-    public int depth = 0;
+    public float frequency = 10f;
 
-    [Range(1,5)]
-    public int octaves = 4;
-    [Range(0,1)]
-    public float persistance = 0.5f;
-    [Range(0, 3)]
+    [Range(1, 8)]
+    public int octaves = 2;
+
+    [Range(1f, 4f)]
     public float lacunarity = 2f;
 
-    private float[,] map;
-    
-	void Update () {
-        GameObject quad = GameObject.Find("Quad");
-        Renderer renderer = quad.GetComponent<Renderer>();
-        GenerateMap();
-        renderer.material.mainTexture = GenerateTexture();
-	}
+    [Range(0f, 1f)]
+    public float persistence = 0.5f;
 
-    void GenerateMap() {
+    [Range(1, 3)]
+    public int dimensions = 3;
 
-        map = new float[width, height];
+    public NoiseMethodType type;
 
-        Vector2[] octaveOffSets = new Vector2[octaves];
-        System.Random prng = new System.Random(seed);
-        for (int i =0; i < octaves; i++) {
-            float offSetX = prng.Next(-10000, 10000);
-            float offSetY = prng.Next(-10000, 10000);
-            octaveOffSets[i] = new Vector2(offSetX, offSetY);
+    private Texture2D texture;
+
+    private void OnEnable() {
+        if (texture == null) {
+            texture = new Texture2D(resolution, resolution, TextureFormat.RGB24, true);
+            texture.name = "Procedural Texture";
+            texture.wrapMode = TextureWrapMode.Clamp;
+            texture.filterMode = FilterMode.Trilinear;
+            texture.anisoLevel = 9;
+            GetComponent<MeshRenderer>().material.mainTexture = texture;
+            FillTexture();
         }
-
-        float maxNoiseHeight = float.MinValue;
-        float minNoiseHeight = float.MaxValue;
-
-        for (int x = 0; x < width; x++) {
-            for(int y = 0; y < height; y++) {
-
-                float amplitude = 1;
-                float frequency = 1;
-                float noiseHeight = 0;
-
-                for (int i = 0; i < octaves; i++) {
-                    float sampleX = x / scale * frequency + octaveOffSets[i].x;
-                    float sampleY = y / scale * frequency + octaveOffSets[i].y;
-
-                    float noiseValue = Mathf.PerlinNoise(sampleX, sampleY) * maxDepth + depth;
-                    noiseHeight += noiseValue * amplitude;
-
-                    amplitude *= persistance;
-                    frequency *= lacunarity;
-                }
-
-                if (noiseHeight > maxNoiseHeight) {
-                    maxNoiseHeight = noiseHeight;
-                } else if (noiseHeight < minNoiseHeight) {
-                    minNoiseHeight = noiseHeight;
-                }
-
-                map[x, y] = noiseHeight;
-            }
-        }
-        //for (int y = 0; y < height; y++) {
-        //    for (int x = 0; x < width; x++) {
-        //        map[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, map[x, y]);
-        //    }
-        //}
-
     }
 
-    Texture GenerateTexture() {
-        Texture2D texture = new Texture2D(width, height);
+    private void Update() {
+        if (transform.hasChanged) {
+            transform.hasChanged = false;
+            FillTexture();
+        }
+    }
 
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                Color color = TerrainType(x,y);
-                texture.filterMode = FilterMode.Point;
-                texture.wrapMode = TextureWrapMode.Clamp;
-                texture.SetPixel(x, y, color);
-            }
+    public void FillTexture() {
+        if (texture.width != resolution) {
+            texture.Resize(resolution, resolution);
         }
 
+        Vector3 point00 = transform.TransformPoint(new Vector3(-0.5f, -0.5f));
+        Vector3 point10 = transform.TransformPoint(new Vector3(0.5f, -0.5f));
+        Vector3 point01 = transform.TransformPoint(new Vector3(-0.5f, 0.5f));
+        Vector3 point11 = transform.TransformPoint(new Vector3(0.5f, 0.5f));
+
+        NoiseMethod method = Noise.noiseMethods[(int)type][dimensions - 1];
+        float stepSize = 1f / resolution;
+        for (int y = 0; y < resolution; y++) {
+            Vector3 point0 = Vector3.Lerp(point00, point01, (y + 0.5f) * stepSize);
+            Vector3 point1 = Vector3.Lerp(point10, point11, (y + 0.5f) * stepSize);
+            for (int x = 0; x < resolution; x++) {
+                Vector3 point = Vector3.Lerp(point0, point1, (x + 0.5f) * stepSize);
+                float sample = Noise.Sum(method, point, frequency, octaves, lacunarity, persistence);
+                if (type != NoiseMethodType.Value) {
+                    sample = sample * 0.5f + 0.5f;
+                }
+                texture.SetPixel(x, y, Color.white * sample);
+            }
+        }
         texture.Apply();
-        return texture;
-    }
-
-    Color TerrainType(int x, int y) {
-        float type = map[x, y];
-
-        if(type <= 0.4 * maxDepth) {
-            return Color.blue;
-        }
-        if (type <= 0.6 * maxDepth) {
-            return Color.green;
-        }
-        if (type <= 0.8 * maxDepth) {
-            return Color.black;
-        }
-        return Color.white;
     }
 }
