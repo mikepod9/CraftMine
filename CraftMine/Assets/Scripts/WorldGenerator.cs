9bf7cc15b4d19c822fe60a2810ad86918268a7e6
mikepod9 <mikepodimov@gmail.com>
2017-09-17 15:18:32 -0400

    Added proper simple layer generation using a 2D noise map
    Ignore previous commits. This is the initial commit for world generation.

--- CraftMine/Assets/Scripts/WorldGenerator.cs
+++ CraftMine/Assets/Scripts/WorldGenerator.cs
@@ -11,8 +11,15 @@ public class WorldGenerator : MonoBehaviour {
 
     public colorType colortype;
 
-    [Range(2, 512)]
-    public int resolution = 128;
+    [Range(2, 128)]
+    public int length = 64;
+
+    public int depth = 128;
+
+    [Range(0, 128)]
+    public int z = 0;
+
+    public int waterLevel = 64;
 
     public float frequency = 10f;
 
@@ -26,24 +33,15 @@ public class WorldGenerator : MonoBehaviour {
     public float persistence = 0.5f;
 
     [Range(1, 3)]
-    public int dimensions = 3;
+    public int dimensions = 2;
 
     public NoiseMethodType type;
 
-    public TerrainType[] blocks;
-
     private Texture2D texture;
-
-    [System.Serializable]
-    public struct TerrainType {
-        public string name;
-        public float cutOffHeight;
-        public Color color;
-    }
 
     private void OnEnable() {
         if (texture == null) {
-            texture = new Texture2D(resolution, resolution, TextureFormat.RGB24, true);
+            texture = new Texture2D(length, length, TextureFormat.RGB24, true);
             texture.name = "Procedural Texture";
             texture.wrapMode = TextureWrapMode.Clamp;
             texture.filterMode = FilterMode.Point;
@@ -61,8 +59,8 @@ public class WorldGenerator : MonoBehaviour {
     }
 
     public void FillTexture() {
-        if (texture.width != resolution) {
-            texture.Resize(resolution, resolution);
+        if (texture.width != length) {
+            texture.Resize(length, length);
         }
 
         Vector3 point00 = transform.TransformPoint(new Vector3(-0.5f, -0.5f));
@@ -71,18 +69,19 @@ public class WorldGenerator : MonoBehaviour {
         Vector3 point11 = transform.TransformPoint(new Vector3(0.5f, 0.5f));
 
         NoiseMethod method = Noise.noiseMethods[(int)type][dimensions - 1];
-        float stepSize = 1f / resolution;
-        for (int y = 0; y < resolution; y++) {
+        float stepSize = 1f / length;
+        for (int y = 0; y < length; y++) {
             Vector3 point0 = Vector3.Lerp(point00, point01, (y + 0.5f) * stepSize);
             Vector3 point1 = Vector3.Lerp(point10, point11, (y + 0.5f) * stepSize);
-            for (int x = 0; x < resolution; x++) {
+            for (int x = 0; x < length; x++) {
                 Vector3 point = Vector3.Lerp(point0, point1, (x + 0.5f) * stepSize);
                 float sample = Noise.Sum(method, point, frequency, octaves, lacunarity, persistence);
                 if (type != NoiseMethodType.Value) {
                     sample = sample * 0.5f + 0.5f;
                 }
+                sample *= depth;
                 if (colortype == colorType.Colors) {
-                    texture.SetPixel(x, y, BlockColor(sample));
+                    texture.SetPixel(x, y, BlockColor(sample, z));
                 } else {
                     texture.SetPixel(x, y, Color.white * sample);
                 }
@@ -92,15 +91,21 @@ public class WorldGenerator : MonoBehaviour {
         texture.Apply();
     }
 
-    private Color BlockColor(float noiseSample) {
-        Color color = new Color();
+    private Color BlockColor(float noiseSample, int z) {
+        Color color = Color.white;
 
-        for (int i = 0; i < blocks.Length; i++) {
-            if (noiseSample <= blocks[i].cutOffHeight) {
-                color = blocks[i].color;
-                break;
-            }
-        }
+        int dirtLayer = Mathf.FloorToInt(noiseSample);
+        int dirtThickness = dirtLayer / 32 - 6;
+        int stoneLayer = dirtLayer + dirtThickness;
+
+        if (z == 0)
+            color = Color.black;
+        else if (z <= stoneLayer)
+            color = Color.gray;
+        else if (z <= dirtLayer)
+            color = Color.green;
+
+
         return color;
     }
 
