215e035d94ea86032d67002ad7bfb10224cf9f43
mikepod9 <mikepodimov@gmail.com>
2017-09-17 16:45:35 -0400

    Tested 3D terrain generation using very unoptemized method of spawing cubes
    Looks good though!

--- CraftMine/Assets/Scripts/WorldGenerator.cs
+++ CraftMine/Assets/Scripts/WorldGenerator.cs
@@ -37,7 +37,16 @@ public class WorldGenerator : MonoBehaviour {
 
     public NoiseMethodType type;
 
+    public Material Air;
+    public Material Land;
+    public Material Stone;
+    public Material Bedrock;
+
     private Texture2D texture;
+
+    private void Start() {
+        SpawnBlock();
+    }
 
     private void OnEnable() {
         if (texture == null) {
@@ -47,15 +56,62 @@ public class WorldGenerator : MonoBehaviour {
             texture.filterMode = FilterMode.Point;
             texture.anisoLevel = 9;
             GetComponent<MeshRenderer>().material.mainTexture = texture;
-            FillTexture();
+            //FillTexture();
         }
     }
 
     private void Update() {
         if (transform.hasChanged) {
             transform.hasChanged = false;
-            FillTexture();
+            //FillTexture();
         }
+    }
+
+    public void SpawnBlock() {
+        Vector3 point00 = transform.TransformPoint(new Vector3(-0.5f, -0.5f));
+        Vector3 point10 = transform.TransformPoint(new Vector3(0.5f, -0.5f));
+        Vector3 point01 = transform.TransformPoint(new Vector3(-0.5f, 0.5f));
+        Vector3 point11 = transform.TransformPoint(new Vector3(0.5f, 0.5f));
+
+        NoiseMethod method = Noise.noiseMethods[(int)type][dimensions - 1];
+        float stepSize = 1f / length;
+        for (int y = 0; y < length; y++) {
+            Vector3 point0 = Vector3.Lerp(point00, point01, (y + 0.5f) * stepSize);
+            Vector3 point1 = Vector3.Lerp(point10, point11, (y + 0.5f) * stepSize);
+            for (int x = 0; x < length; x++) {
+                Vector3 point = Vector3.Lerp(point0, point1, (x + 0.5f) * stepSize);
+                float sample = Noise.Sum(method, point, frequency, octaves, lacunarity, persistence);
+                if (type != NoiseMethodType.Value) {
+                    sample = sample * 0.5f + 0.5f;
+                }
+                sample *= depth;
+                //PlaceBlock(sample, x, y, 64);
+                for (int i = 50; i < depth; i++) {
+                    PlaceBlock(sample, x, y, i);
+                }
+            }
+        }
+    }
+
+    private void PlaceBlock(float noiseSample, int x, int y, int z) {
+
+        int dirtLayer = Mathf.FloorToInt(noiseSample);
+        int dirtThickness = dirtLayer / 32 - 6;
+        int stoneLayer = dirtLayer + dirtThickness;
+
+        if (z == 0) {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.GetComponent<MeshRenderer>().material = Bedrock;
+            cube.transform.position = new Vector3(x, y, z);
+        } else if (z <= stoneLayer) {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.GetComponent<MeshRenderer>().material = Stone;
+            cube.transform.position = new Vector3(x, y, z);
+        } else if (z <= dirtLayer) {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.GetComponent<MeshRenderer>().material = Land;
+            cube.transform.position = new Vector3(x, y, z);
+        } else return;
     }
 
     public void FillTexture() {
